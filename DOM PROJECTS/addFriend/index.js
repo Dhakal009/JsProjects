17376143c689c00050232a304e84712429d4effb
Bikash Dhakal <bikashdhakal546@gmail.com>
2026-03-21 10:03:37 +0545

    dom addfriend feature

---
+++ DOM PROJECTS/addFriend/index.js
@@ -0,0 +1,23 @@
+let addFriend = document.getElementById("add");
+let removeFriend = document.getElementById("remove");
+
+let name = document.querySelector("h1");
+let nameTag = document.querySelector("h5");
+
+let flag = 0;
+addFriend.addEventListener("click",function(){
+    if (flag === 0){
+        nameTag.textContent = "Friends";
+        nameTag.style.color="Green"
+        addFriend.textContent = "Remove Friend"
+        addFriend.style.backgroundColor = "Blue";
+        flag = 1;
+    }else{
+        nameTag.textContent = "Stranger";
+        nameTag.style.color = "red"
+        addFriend.textContent = "Add Friend"
+        addFriend.style.backgroundColor = "cadetblue"
+        flag = 0;
+    }
+})
+
